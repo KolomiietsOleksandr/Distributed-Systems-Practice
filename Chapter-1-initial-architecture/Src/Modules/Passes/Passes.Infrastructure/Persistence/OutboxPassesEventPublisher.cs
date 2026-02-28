32046f220b59e1ca410790e10d4d5c48ee3dec87
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2026-02-28 03:41:50 +0200

    Pr3 Fix

--- Chapter-1-initial-architecture/Src/Modules/Passes/Passes.Infrastructure/Persistence/OutboxPassesEventPublisher.cs
+++ Chapter-1-initial-architecture/Src/Modules/Passes/Passes.Infrastructure/Persistence/OutboxPassesEventPublisher.cs
@@ -1,14 +1,10 @@
-namespace EvolutionaryArchitecture.Fitnet.Passes.Infrastructure.Persistence;
+﻿namespace EvolutionaryArchitecture.Fitnet.Passes.Infrastructure.Persistence;
 
 using System.Text.Json;
 using EvolutionaryArchitecture.Fitnet.Passes.Application;
 using EvolutionaryArchitecture.Fitnet.Passes.MarkPassAsExpired.Events;
 using EvolutionaryArchitecture.Fitnet.Passes.RegisterPass.Events;
 
-/// <summary>
-/// Instead of publishing events directly, we store them in the Outbox.
-/// Later, the Outbox Processor will publish them reliably.
-/// </summary>
 internal sealed class OutboxPassesEventPublisher(IPassesOutboxWriter outboxWriter) : IPassesEventPublisher
 {
     public Task PassRegisteredAsync(Guid passId, DateTimeOffset occurredAt, CancellationToken ct)
